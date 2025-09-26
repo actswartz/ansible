9e563e46e5a09b2a3137dbea110cdf804e573c04
actswartz <j@nr4.com>
2025-09-26 12:41:29 -0400

    up

---
+++ E_DNAC/lab_4.2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "ansible-playbook get_devices.yml"
+ansible-playbook get_devices.yml
---
+++ E_DNAC/lab_6.3.sh
@@ -0,0 +1,3 @@
+#!/bin-bash
+echo "ansible-playbook create_template_report.yml"
+ansible-playbook create_template_report.yml
